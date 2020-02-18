02adc2bb5f4a32f11b186d2e783e7ff4ce901d69
Ondřej Ruml <o.ruml17@gmail.com>
2020-02-18 17:08:14 +0100

    project setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './quasar'
+import DataEditorJSONSchema from '@oarepo/data-editor-jsonschema'
+import DataEditor from '@oarepo/data-editor'
+import DataRenderer from '@oarepo/data-renderer'
 
 Vue.config.productionTip = false
+Vue.use(DataEditorJSONSchema)
+Vue.use(DataEditor)
+Vue.use(DataRenderer)
 
 new Vue({
   router,
